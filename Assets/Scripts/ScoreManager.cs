2b1ecc25da3f5383c4782da0971e748c5e368907
Yam Gomes <yamgcunha@gmail.com>
2022-06-14 14:44:25 -0300

    added persistance to file

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System;
 
 public class ScoreManager : MonoBehaviour
 {
+    [Serializable]
+    public struct Player
+    {
+        public string name;
+        public int score;
+
+        public Player(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+    }
+
+    public GameObject nameInput;
+
     public static ScoreManager instance;
 
     List<Player> players = new List<Player>();
+
+    Text scoreboard;
 
     private void Awake()
     {
@@ -18,45 +36,63 @@ public class ScoreManager : MonoBehaviour
 
     void Start()
     {
-        LoadScores();
+        scoreboard = GetComponent<Text>();
+        if (!File.Exists("./scores.dat"))
+        {
+            Player p = new Player("Noob", 10);
+            AddPlayer(p);
+            p = new Player("Pro", 100);
+            AddPlayer(p);
+            p = new Player("Master", 1000);
+            AddPlayer(p);
+            p = new Player("Legend", 10000);
+            AddPlayer(p);
+            p = new Player("God", 100000);
+            SaveScores();
+            Debug.Log("Scores saved");
+        }
+        else
+        {
+            Debug.Log("Scores loaded");
+            LoadScores();
+        }
     }
 
     void SaveScores()
     {
+        //save scores to file using BinaryFormatter
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/scores/score.dat");
+        FileStream file = File.Create("./scores.dat");
         bf.Serialize(file, players);
         file.Close();
     }
 
     void LoadScores()
     {
-        if (File.Exists(Application.persistentDataPath + "/scores.dat"))
+        //load scores from file using BinaryFormatter
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open("./scores.dat", FileMode.Open);
+        players = (List<Player>)bf.Deserialize(file);
+        file.Close();
+        scoreboard.text = "";
+        foreach (Player p in players)
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(
-                Application.persistentDataPath + "/scores.dat",
-                FileMode.Open
-            );
-            List<Player> data = (List<Player>)bf.Deserialize(file);
-            players = new List<Player>(data);
-            file.Close();
+            scoreboard.text += p.name + " - " + p.score + "\n";
         }
-        else
-            Debug.LogError("No scores!");
     }
 
-    void NewScore()
+    public void EndScoreAdd()
     {
-        Player player = new Player();
-        player.name = "Player";
-        player.score = 0;
-        players.Add(player);
+        Player p = new Player();
+        p.name = nameInput.GetComponent<InputField>().text;
+        p.score = GameManager.instance.score;
+        AddPlayer(p);
+        SaveScores();
     }
-}
 
-class Player
-{
-    public string name;
-    public int score;
+    void AddPlayer(Player player)
+    {
+        players.Add(player);
+        players.Sort((x, y) => y.score.CompareTo(x.score));
+    }
 }
